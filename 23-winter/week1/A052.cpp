490c2e33f7b82b089ff0d61aba2897a261b90c1f
Eziwon <108611096+Eziwon@users.noreply.github.com>
2024-01-22 23:03:15 +0900

    Create A052.cpp

---
+++ 23-winter/week1/A052.cpp
@@ -0,0 +1,33 @@
+// BaekJoon
+// 문제: 8958. OX퀴즈
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    cin.ignore();
+    vector<string> ans(N);
+    
+    for (int i = 0; i < N; i++) {
+        getline(cin, ans[i]);
+    }
+    
+    for (int i = 0; i < N; i++) {
+        int score = 1;
+        int total = 0;
+        for (char ch : ans[i]) {
+            if (ch == 'X') {
+                score = 0;
+            }
+            total += score;
+            score++;
+        }
+        
+        cout << total << endl;
+    }
+    
+    return 0;
+}
