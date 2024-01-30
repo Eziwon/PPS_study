97d0d988baa51531c5f2486173efd6486ce23368
Eziwon <108611096+Eziwon@users.noreply.github.com>
2024-01-30 18:49:20 +0900

    Delete 23-winter/week3/C006.py

--- 23-winter/week3/C006.py
+++
@@ -1,9 +0,0 @@
-# LeetCode
-# 387. First Unique Character in a String
-
-class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        for i, c in enumerate(s):
-            if s.count(c) == 1:
-                return i
-        return -1
