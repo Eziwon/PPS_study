b74ba6ef9acaa6a3d03ee84866a54160bad2e0fc
Eziwon <108611096+Eziwon@users.noreply.github.com>
2024-02-05 20:22:53 +0900

    Create B081.py

---
+++ 23-winter/week3/B081.py
@@ -0,0 +1,17 @@
+# LeetCode
+# 1646. Get Maximum in Generated Array
+
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        nums = []
+        nums.append(0)
+        nums.append(1)
+        for i in range(2, n+1):
+            if i%2 == 0:
+                nums.append(nums[i//2])
+            else:
+                nums.append(nums[i//2] + nums[(i//2) + 1])
+        if n == 0:
+            return 0
+        return max(nums)
+      
