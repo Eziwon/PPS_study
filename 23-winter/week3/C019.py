42a434b7d3bf8c3f773a0599ed605b4146ae546b
Eziwon <108611096+Eziwon@users.noreply.github.com>
2024-02-05 20:29:05 +0900

    Create C019.py

---
+++ 23-winter/week3/C019.py
@@ -0,0 +1,9 @@
+# LeetCode
+# 242. Valid Anagram
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sort_s = sorted(s)
+        sort_t = sorted(t)
+        return sort_s == sort_t
+      
