d47c5adf8b4b0487bebe1c4011090631dcdd528b
Eziwon <108611096+Eziwon@users.noreply.github.com>
2024-02-13 23:07:34 +0900

    Create 10926.cpp

---
+++ BaekJoon/10926.cpp
@@ -0,0 +1,16 @@
+// ??!
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string id;
+    getline(cin, id);
+    
+    string res = id + "??!";
+    
+    cout << res << endl;
+    
+    return 0;
+}
