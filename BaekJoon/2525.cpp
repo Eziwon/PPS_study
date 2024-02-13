e6a6f8a4b2ea42edf16bdf42fb93bf5b5c3caccb
Eziwon <108611096+Eziwon@users.noreply.github.com>
2024-02-13 22:59:45 +0900

    Create 2525.cpp

---
+++ BaekJoon/2525.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int h, m, t;
+    cin >> h >> m >> t;
+    
+    m = m + t;
+    while(m >= 60) {
+        m -= 60;
+        h += 1;
+        if (h == 24) h = 0;
+    }
+    cout << h << " " << m << endl;
+    
+    return 0;
+}
