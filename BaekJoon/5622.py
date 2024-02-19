def7a735f23f86bc98dd82a383aa93b61e89b5d4
Eziwon <108611096+Eziwon@users.noreply.github.com>
2024-02-19 21:27:17 +0900

    Create 5622.py

---
+++ BaekJoon/5622.py
@@ -0,0 +1,12 @@
+# 다이얼
+
+import sys
+
+dial=['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+a=input()
+ans=0
+for i in range(len(a)):
+    for j in dial:
+        if a[i] in j:
+            ans+=dial.index(j)+3
+print(ans)
