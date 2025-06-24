f18384153eebe42551d6bd241a36385a7b5e43aa
Eziwon <eziwon0228@gmail.com>
2025-06-24 15:56:41 +0900

    1655 파이썬

---
+++ BaekJoon/1655/1655.py
@@ -0,0 +1,25 @@
+import sys
+import heapq
+
+input = sys.stdin.readline  # 이 코드 추가로 시간초과 문제 해결
+
+N = int(input())
+
+max_heap = []
+min_heap = []
+
+for _ in range(N):
+    num = int(input())
+
+    if (len(max_heap) == len(min_heap)):
+        heapq.heappush(max_heap, -num)
+    else:
+        heapq.heappush(min_heap, num)
+    
+    if min_heap and -max_heap[0] > min_heap[0]:
+        max_val = -heapq.heappop(max_heap)
+        min_val = heapq.heappop(min_heap)
+        heapq.heappush(max_heap, -min_val)
+        heapq.heappush(min_heap, max_val)
+    
+    print(-max_heap[0])
