fc14001f20672d0ffa9a4886728bfa8ed95f3c8f
Eziwon <eziwon0228@gmail.com>
2025-07-10 18:41:01 +0900

    5086.py complete

---
+++ BaekJoon/5086/5086.py
@@ -0,0 +1,13 @@
+
+while(1):
+    n1, n2 = map(int, input().split())
+
+    if n1 == 0 and n2 == 0:
+        break
+
+    if n2 % n1 == 0 and n1 < n2:
+        print("factor")
+    elif n1 % n2 == 0 and n1 > n2:
+        print("multiple")
+    else:
+        print("neither")
